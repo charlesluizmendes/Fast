b12fd68c5e7675b0af653fac9d2e8cf75f11f17b
Charles Mendes <charlesluizmendes@gmail.com>
2025-02-15 14:42:42 -0300

    Especificando DTO de response do UserLogin

--- src/api/routes/user_routes.py
+++ src/api/routes/user_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from src.application.services.user_service import UserService
-from src.application.dtos.user_dto import UserCreateDTO, UserResponseDTO, UserLoginDTO
+from src.application.dtos.user_dto import UserCreateDTO, UserResponseDTO, UserLoginDTO, UserLoginResponseDTO
 from src.api.ioc import get_user_service
 
 
@@ -12,7 +12,7 @@ async def create_user(user: UserCreateDTO,
         service: UserService = Depends(get_user_service)):
     return service.create_user(user)
 
-@router.post("/login/")
+@router.post("/login/", response_model=UserLoginResponseDTO)
 async def login_user(user: UserLoginDTO, 
         service: UserService = Depends(get_user_service)):
     return service.login_user(user)
